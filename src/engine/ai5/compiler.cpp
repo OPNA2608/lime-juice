9252be0049c7a5105495956192ca0dd42008d18e
OPNA2608 <opna2608@protonmail.com>
2026-04-13 18:12:50 +0200

    treewide: Add GPL-3.0-or-later copyright headers to code files

--- src/engine/ai5/compiler.cpp
+++ src/engine/ai5/compiler.cpp
@@ -1,3 +1,21 @@
+//
+// lime-juice: C++ port of Tomyun's "Juice" de/recompiler for PC-98 games
+// Copyright (C) 2026 Fuzion
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
 #include "compiler.h"
 #include "tokens.h"
 #include "../ai1/compiler.h"
